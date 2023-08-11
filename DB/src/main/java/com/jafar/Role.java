9a5aec5df49b50f67b83f1317baf36ca698bda44
JafarJLabiba <jafar@labiba.ai>
2023-08-11 05:44:04 +0300

    changed Role DAO to record

--- DB/src/main/java/com/jafar/Role.java
+++ DB/src/main/java/com/jafar/Role.java
@@ -2,12 +2,5 @@ package com.jafar;
 
 import java.io.Serializable;
 
-public class Role implements Serializable {
-    private String id;
-    private String name;
-
-    public Role(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+public record Role(String id, String name) implements Serializable {
 }
